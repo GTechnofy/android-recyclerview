bff34898942a821dce1b7d71ff4f96e47d2fff17
ggupta <grvgupta999@gmail.com>
2017-08-31 00:40:17 +0530

    Adding MainActivity

---
+++ app/src/main/java/com/gtechnofy/sample/recyclerview/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gtechnofy.sample.recyclerview.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gtechnofy.sample.recyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
